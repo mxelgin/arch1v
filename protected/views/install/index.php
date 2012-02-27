42ef601721dbe0ceab7caf20cc6572d96c330336
mxelgin <mxelgin@yandex.ru>
2012-02-28 04:01:57 +0600

    Добавлен MVC Install *теперь установка описывается тут http://localhost/arch1v/index.php?r=install

---
+++ protected/views/install/index.php
@@ -0,0 +1,12 @@
+<?php
+$this->breadcrumbs=array(
+	'Install',
+);?>
+
+<p>
+	1.	В папке arch1v\protected\config\main.php в папраметре db изменить connectionstring путь до БД 
+</p>
+<p>
+	2.	В папке arch1v\protected\data\model.sql запустить скрипт
+</p>
+
